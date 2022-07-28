567a9050495eae74b8770930aa5e9dd9bbea1bcd
GickiAnarchy <mindofmagick85@gmail.com>
2022-07-28 18:36:50 -0500

    dwyt additiions; save list

--- DownWithYT/v0.2/dwyt.py
+++ DownWithYT/v0.2/dwyt.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os, time, sys
 from pytube import YouTube, Playlist, Channel, Search
 
 MagickChannel = "https://youtube.com/user/MindofMagick"
@@ -10,54 +9,107 @@ class DownWithYT:
         self.search = None
         self.urls = []
         self.suggestions = []
+        self.song = None
+        self.save_file = "./.saved.json"
+        self.save_list = []
 
 
-    def test_search(self):
-        options = ["1: Search YouTube", "2: Show Results", "3: Get more results", "4: Show Suggestions", "5: Exit"]
-        while True:
-            for option in options:
-                print(option)
-            action = input("What will you do?")
-            if action == "1":
-                self.search_input()
-            if action == "2":
-                self.show_results()
-            if action == "3":
-                self.search_more()
-                self.show_results()
-            if action == "4":
-                self.show_suggestions()
-            if action == "5":
-                break
-        sys.exit()
+#
+#    SAVE FILE AND LIST
+#
+    def check_save_file(self, create = False):
+        exist = os.path.exists(self.save_file)
+        if exist:
+            ret = True
+        if not exist:
+            if create:
+                with open(self.save_file, "w+") as f:
+                    f.close()
+            ret = False
+        return ret
+
+    def read_saved(self):
+        self.check_save_file(True)
+        with open(self.save_file, "r") as savefile:
+            for item in savefile:
+                if YouTube(item) in self.save_list:
+                    print(f"skipped loading {YouTube(item).title}")
+                    continue
+                self.save_list.append(YouTube(item))
+            savefile.close()
+        print(f"{str(len(self.save_list))}")
+        print("lOaDeD")
+
+    def write_saved(self):
+        urls = []
+        for s in self.save_list:
+            urls.append(s.watch_url)
+        with open(self.save_file, "w+") as savefile:
+            for item in self.save_list:
+                savefile.write(item.watch_url)
+            savefile.close()
+        print("SaVeD")
+
+    def add_to_saved(self, song: YouTube):
+        self.read_saved()
+        self.save_list.append(song)
+        print("aDdEd")
+        self.write_saved()
+
+    def show_saved(self):
+        os.system("clear")
+        print("SAVED SONGS")
+        self.read_saved()
+        for song in self.save_list:
+            print(f"{song.title}\n\t{song.watch_url}")
+            time.sleep(.2)
+        input()
+
 
 #
 #    SEARCHING
 #
     def get_results(self):
         return self.results
-    
+
     def show_results(self):
         for res in self.results:
             print(f"{str(self.results.index(res))}: {res.title}")
+        poss = input("Select a song # to choose.\n")
+        if poss.isdigit() and int(poss) <= len(self.results):
+            self.song = self.results[int(poss)]
+            AV = input("Press /'a/' for Audio or /'v/' for Video:\n\"add\" to add to save_list")
+            if AV.lower() == "add":
+                self.add_to_saved(self.song)
+                self.show_saved()
+                return
+            if AV.lower() == "a":
+                good = self.download_audio(self.song)
+            if AV.lower() == "v":
+                good = self.download_video(self.song)
+            if AV.lower() not in ("v", "a"):
+                good = False
+            if good:
+                print(f"{self.song.title} downloaded")
+            if not good:
+                print(f"{self.song.title} failed during download")
+
 
     def search_input(self):
         answer = input("What do you want to search for?\n")
         self.search_yt(answer)
 
     def show_suggestions(self):
-        print("SUGGESTIONS")
+        print("SUGGESTIONS") 
+        self.suggestions = self.search.completion_suggestions
         for sug in self.suggestions:
             num = self.suggestions.index(sug)
             print(f"{str(num)}: {sug}")
             print(type(sug))
 
-    def search_yt(self, msg1):
-        msg = msg1.replace(" ", "+")
-        print(msg)
+    def search_yt(self, msg):
         self.search = Search(msg)
         self.results = self.search.results
-        self.suggestions += self.search.completion_suggestions
 
     def search_more(self):
         self.results.clear()
@@ -67,14 +119,12 @@ class DownWithYT:
 #
 #    DOWNLOADING
 #
-    def download_audio(self, link):
-        yt = YouTube(link)
+    def download_audio(self, yt):
         audio = yt.streams.get_audio_only()
         destination = "."
         try:
             out_file = audio.download(output_path=destination)
         except:
-            err_link = f"::ERROR::\n{link}"
             return False
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
@@ -82,15 +132,12 @@ class DownWithYT:
         name = os.path.split(new_file)
         return True
 
-
-    def download_video(self, link):
-        yt = YouTube(link)
+    def download_video(self, yt):
         video = yt.streams.first()
         destination = "."
         try:
             out_file = video.download(output_path=destination)
         except:
-            err_link = f"::ERROR::\n{link}"
             return False
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp4'
@@ -98,7 +145,60 @@ class DownWithYT:
         name = os.path.split(new_file)
         return True
 
+#
+#    MAIN   
+#
+    def test_main(self):
+        options = ["1: Search YouTube", "2: Show Results", "3: Get more results", "4: Show Suggestions", "5: Change Directory", "6: Up a Directory", "0: Exit"]
+        i = 0
+        while True:
+            if i == 1:
+                os.system("clear")
+                i = 0
+            print("DOWN WITH YOUTUBE :: FatherAnarchy")
+            print(f"\n\tSave File Present?: {self.check_save_file()}")
+            if self.check_save_file():
+                print("\t\t Enter \"read\" or \"r\" to load the saved file.\n")
+            for option in options:
+                print(option)
+            action = input("What will you do?")
+            if action == "1":
+                self.search_input()
+                self.show_results()
+            if action == "2":
+                self.show_results()
+            if action == "3":
+                self.search_more()
+                self.show_results()
+            if action == "4":
+                self.show_suggestions()
+            if action == "5":
+                dir_list = []
+                for item in os.listdir("."):
+                    if os.path.isdir(item):
+                        dir_list.append(item)
+                print(f"Choose a directory:\n{os.getcwd()}/*****")
+                for d in dir_list:
+                    print(f"{dir_list.index(d)}: {d}")
+                choice = input("Enter # of directory")
+                if choice.isdigit():
+                    if len(dir_list) >= int(choice):
+                        os.chdir(dir_list[int(choice)])
+            if action == "6":
+                os.chdir("..")
+            if action.lower() in ("r", "read") and self.check_save_file():
+                self.read_saved()
+                self.show_saved()
+            if action == "0":
+                break
+            i += 1
 
+        sys.exit()
+
+
+#
+#
+#
 if __name__ == "__main__":
     dwyt = DownWithYT()
-    dwyt.test_search()
+    dwyt.test_main()
---
+++ Misc/Python References/csv_ref.py
@@ -0,0 +1,25 @@
+import csv
+
+with open('eggs.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
+
+with open('eggs.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+    
+
+
+with open('names.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['first_name'], row['last_name'])
+        
+with open('names.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['first_name'], row['last_name'])
+
