776f3551f2c8f503d4a162dd7bd4c1fed90713ed
GickiAnarchy <mindofmagick85@gmail.com>
2022-08-10 02:14:31 -0500

    sync

--- DownWithYT/v02/gui.py
+++ DownWithYT/v02/gui.py
@@ -9,8 +9,27 @@ class DWYT_GUI:
 
 
     def makeLayouts(self):
-        self.main_buttons = [sg.Button("Save", k = "btn_save"), sg.Button("Load", k = "btn_load"), sg.Button("Quit", k = "btn_quit")]
+        main_buttons = [sg.Button("Save", k = "btn_save"), sg.Button("Load", k = "btn_load"), sg.Button("Quit", k = "btn_quit")]
+        
+        search_layout = None
 
+        home_layout = None
+
+        saved_layout = None
+
+        about_layout = None
+
+
+        layout = [[]]
+        layout += [[sg.TabGroup(
+                            [[sg.Tab("Home", home_layout),
+                            sg.Tab("Search", search_layout),
+                            sg.Tab("Saved List", saved_layout),
+                            sg.Tab("About", about_layout)]],
+                        key = "tabs", expand_x = True, expand_y = True),
+                        ]]
+
+        layout[-1].append(sg.Sizegrip())
 
 if __name__ == "__main__":
     pass
