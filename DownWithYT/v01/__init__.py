794b382fe3ae1e2d257bf445b29d8ab1870709c0
GickiAnarchy <mindofmagick85@gmail.com>
2022-10-04 22:44:46 -0500

    NA

--- DownWithYT/v01/__init__.py
+++
@@ -1,4 +0,0 @@
-from .linklist import LinkList
-from .base64images import DWY_LOGO, DL_BTN
-
-__all__ = ["LinkList", "base64images.DWY_LOGO","base64images.DL_BTN"]
--- DownWithYT/v03/testing.py
+++
@@ -1,4 +0,0 @@
-import os
-from v03.yt_gui import YT_GUI
-
-YT_GUI.faask()
