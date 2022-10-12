b056a8ac1c1c5665c8a270fdde3604c9c7f27346
GickiAnarchy <mindofmagick85@gmail.com>
2022-10-12 00:13:10 -0500

    neg

--- DownWithYT/v1.1/dwytapp.py
+++ DownWithYT/v1.1/dwytapp.py
@@ -64,7 +64,7 @@ class YT_Widget(BoxLayout):
 
 ##    ##    ##    ##    ##    ##    ##    ##    ##    ##    ##    ##
 class Ask(Popup):
-    
+    pass
 
 
 ##    ##    ##    ##    ##    ##    ##    ##    ##    ##    ##    ##
